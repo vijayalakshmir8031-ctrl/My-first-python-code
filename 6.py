28526f286c62cb7b2648eaaa10ac917b4bb6085a
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2025-11-01 00:09:04 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,45 @@
+# tuple
+my_name = ("element1", "element2", "element3")
+print(type(my_name))
+#set
+my_set = {"element", "element", "element"}
+print(type(my_set))
+#empty set
+empty_set = {"element1", "element2", "element3"}
+empty_set = set()
+print(empty_set)
+#set operation
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union_set = set1 | set2 #union
+print(set1 | set2)
+#intersection
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+intersection_set = set1 & set2
+print(set1 & set2)
+#difference
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+difference_set = set1 - set2
+print(set1 - set2)
+#symmetric difference
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+sym_diff_set = set1 ^ set2
+print(set1 ^ set2)
+#set method
+family_set= {"shreedhar", "sindhu", "vijayalakshmi", "tarun"}
+family_set.add("trisha")
+print(family_set)
+#remove
+family_set= {"shreedhar", "sindhu", "vijayalakshmi", "tarun"}
+family_set.remove("tarun")
+print(family_set)
+#discard
+family_set= {"shreedhar", "sindhu", "vijayalakshmi", "tarun"}
+family_set.discard("tarun")
+print(family_set)
+family_set= {"shreedhar", "sindhu", "vijayalakshmi", "tarun"}
+family_set.clear()
+print(family_set)
