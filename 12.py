a2af348a8d93c275af066b6d7c76f62dcd7daf52
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-02-04 19:52:39 +0530

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,28 @@
+#The if statement
+#if condition:the code block execute if the condition is true
+x = 20
+if x==20:
+     print("number is even")
+#The else statement
+x = 20
+if x==20:
+     print("number is even")
+else:
+     print("number is odd")
+#the elif statement
+time = 3
+
+if time==9:
+     print("it is a breakfast time")
+elif time==1:
+     print("it is a lunch time")
+elif time==9:
+     print("it is a dinner time")
+else:
+     print("it is not a meal time")
+#comparison operation in if statement
+age = 20
+if age>=18:
+     print("you are eligible to vote")
+else:
+     print("you are not eligible to vote")
