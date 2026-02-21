d6cf7d908682a188edc72290778d0c7cf914a87d
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-02-22 00:10:44 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,21 @@
+marks = []
+for i in range(1, 6):
+    mark = float(input(f"Enter marks for subject {i}: "))
+    marks.append(mark)
+
+total = sum(marks)
+average = total / 5
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "Fail"
+print("Total =", total)
+print("Average =", average)
+print("Grade =", grade)
+
