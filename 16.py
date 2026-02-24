7e21dde184cbecdc17d50d8cdbb928148ae33ad3
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-02-24 17:49:17 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -4,3 +4,35 @@ if x==20:
   print("x is even")
 else:
   print("x is odd")
+
+2: x = 10
+
+if x > 0:
+    print("it is +ve")
+elif x < 0:
+    print("it is -ve")
+else:
+    print("it is zero")
+
+3: x = 110
+y = 20
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+
+4: age = 18
+
+if age >= 18:
+    print("eligible to vote")
+else:
+    print("not eligible")
+
+6: marks = 40
+if marks >= 40:
+  print("pass")
+else:
+  print("fail")
