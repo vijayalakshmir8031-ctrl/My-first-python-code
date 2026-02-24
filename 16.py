0565a2e29cf61d77fe15e18930032fef21efb338
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-02-24 19:14:56 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -50,3 +50,13 @@ if x % 3 == 0:
   print("pizza")
 else:
   print("print number")
+
+marks = 100
+if marks > 90:
+  print("A")
+elif marks < 80:
+  print("B")
+elif marks < 70:
+  print("c")
+else:
+  print("Fail")
