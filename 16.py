0b7aace4ca5f6270a3af6f5abaf94d27ac045581
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-02-24 19:15:14 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -60,3 +60,23 @@ elif marks < 70:
   print("c")
 else:
   print("Fail")
+
+x = 100
+y = 20
+z = 30
+
+# Largest
+if x >= y and x >= z:
+    print("x is largest")
+elif y >= x and y >= z:
+    print("y is largest")
+else:
+    print("z is largest")
+
+# Smallest
+if x <= y and x <= z:
+    print("x is smallest")
+elif y <= x and y <= z:
+    print("y is smallest")
+else:
+    print("z is smallest")
