ac5db9822ec533eec08b56c501776419edfe14d9
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-07 21:58:57 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -3,3 +3,10 @@ total = 0
 for num in L:
     total = total + num
     print(total)
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
