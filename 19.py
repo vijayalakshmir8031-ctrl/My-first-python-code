6882d5fc28558fd0dde788a0a5a3a93b45a196b3
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-11 22:10:17 +0530

    Update 19.py

--- 19.py
+++ 19.py
@@ -22,3 +22,8 @@ Example 2: Splitting a string with commas
 data = "apple,banana,mango"
 fruits = data.split(",")
 print(fruits)
+
+Example 3: Limiting the number of splits
+sentence = "Python is fun to learn"
+words = sentence.split(" ", 2)
+print(words)
