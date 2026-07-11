90223b768d5ae10160c52564333131ca5dc0e09a
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-11 22:09:16 +0530

    Update 19.py

--- 19.py
+++ 19.py
@@ -2,3 +2,8 @@ Example 1: Creating a dictionary of squares
 numbers = [1, 2, 3, 4, 5]
 squares_dict = {num: num ** 2 for num in numbers}
 print(squares_dict)
+
+Example 2: Converting a list of names to a dictionary of name lengths
+names = ["Anand", "Geetha", "Kumar"]
+name_lengths = {name: len(name) for name in names}
+print(name_lengths)
