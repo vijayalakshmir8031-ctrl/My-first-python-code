3f526ccebf645787fb391611580076dc7819ce25
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-13 21:23:44 +0530

    Update 19.py

--- 19.py
+++ 19.py
@@ -69,3 +69,14 @@ elif num < 0:
     print("Negative Number")
 else:
     print("Zero")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print("Largest number is:", a)
+elif b >= a and b >= c:
+    print("Largest number is:", b)
+else:
+    print("Largest number is:", c)
