fccd168c291eb6b3d2729dc5029dfeb9b482133a
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-14 20:50:28 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -11,3 +11,10 @@ def tables(num):
   for i in range(1,11):
     print(f"num x i = {num*i}")
   tables(2)
+
+def marriage(boy,girl):
+  print(f" boy is {boy}")
+  print(f" girl is {girl}")
+  print(f" boy married {girl}")
+  
+marriage("chandhan", "sneha")
