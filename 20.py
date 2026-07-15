920d3dfe3300085d0f98869492448954523651f8
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-15 22:22:45 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -47,3 +47,11 @@ student_info(name="Anand", age=22, course="
 
 double = lambda x: x * 2
 print(double(5))
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
