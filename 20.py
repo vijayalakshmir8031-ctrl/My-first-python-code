9a06201fc3be296941d9374fc7cc5f7a60b43106
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-16 21:51:23 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -103,3 +103,18 @@ def factorial(n):
 
 num = int(input("Enter a number: "))
 print("Factorial =", factorial(num))
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+
+if is_prime(num):
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
