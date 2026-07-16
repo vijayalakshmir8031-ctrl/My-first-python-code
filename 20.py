1870e28537a1ec5d346dd68bc38ac3a594dad3ee
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-16 21:50:50 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -80,3 +80,17 @@ def check_even_odd(num):
 
 number = int(input("Enter a number: "))
 print(check_even_odd(number))
+
+def largest(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+print("Largest number is:", largest(x, y, z))
