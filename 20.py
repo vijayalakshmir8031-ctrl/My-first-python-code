c03d9cfa8cb22d28db96eba78a797ee4213472a3
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-20 23:01:19 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -266,3 +266,29 @@ if is_prime(num):
     print("Prime Number")
 else:
     print("Not a Prime Number")
+
+print("===== Student Result =====")
+
+name = input("Enter Student Name: ")
+
+m1 = int(input("Enter Python Marks: "))
+m2 = int(input("Enter Maths Marks: "))
+m3 = int(input("Enter English Marks: "))
+
+total = m1 + m2 + m3
+average = total / 3
+
+print("\nStudent Name:", name)
+print("Total Marks:", total)
+print("Average:", average)
+
+if average >= 90:
+    print("Grade: A+")
+elif average >= 75:
+    print("Grade: A")
+elif average >= 60:
+    print("Grade: B")
+elif average >= 50:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
