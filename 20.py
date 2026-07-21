f82fe036712f242d1cf85669b80769fb86cb47b8
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-21 23:01:50 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -407,3 +407,12 @@ def check_even_odd(number):
 
 num = int(input("Enter a number: "))
 print("The number is", check_even_odd(num))
+
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+num = int(input("Enter a number: "))
+print("Factorial =", factorial(num))
