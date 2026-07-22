43ffae6fff3b1fa6f35cab51a7f5f5f497fca729
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-22 23:09:12 +0530

    Update 21.py

--- 21.py
+++ 21.py
@@ -11,3 +11,18 @@ class Car:
 # Creating an object of the class
 my_car = Car("Toyota", "Corolla")
 my_car.display_info()
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# Creating objects
+person1 = Person("Arjun", 30)
+person2 = Person("Megha", 25)
+
+person1.greet()
+person2.greet()
