d333c515af0e3105ecc975d052295eca79ee32ac
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-23 23:19:27 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -66,3 +66,10 @@ if ch.lower() in "aeiou":
     print("Vowel")
 else:
     print("Consonant")
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
