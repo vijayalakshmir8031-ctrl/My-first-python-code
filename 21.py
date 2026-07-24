3d63a04e06648cc5d6877c2992547e622eed81e9
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-24 23:30:47 +0530

    Update 21.py

--- 21.py
+++ 21.py
@@ -124,3 +124,24 @@ employee3 = employee("sindhu","manager", 60000)
 employee1.display()
 employee2.display()
 employee3.display()
+
+class employee:
+    def __init__(self,name, designation,salary=30000):
+        self.name = name
+        self.designation = designation
+        self.salary = salary
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Designation: {self.designation}")
+        print(f"Salary: {self.salary}")
+
+employee1 = employee("tarun", "developer", 50000)
+
+employee2 = employee("trisha", "designer")
+
+employee3 = employee("sindhu","manager", 60000)
+
+employee1.display()
+employee2.display()
+employee3.display()
