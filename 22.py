b5bc9269f1a713785e7d8d392459476186c6fb1c
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-25 21:22:23 +0530

    Add Database class for data storage functionality

--- 22.py
+++ 22.py
@@ -57,3 +57,18 @@ car = Car()
 car.start_engine()  # Abstracts complex internal workings
 car.accelerate()
 car.brake()
+
+class Database:
+    def __init__(self):
+        self.__storage = {}
+
+    def save_data(self, key, value):
+        self.__storage[key] = value
+        print(f"Data saved for {key}")
+
+    def get_data(self, key):
+        return self.__storage.get(key, "No data found")
+
+db = Database()
+db.save_data("user_101", {"name": "Raj", "age": 30})
+print(db.get_data("user_101"))
