d722dc460baee3b5a36890141b3799bdf9b7ea6d
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-25 21:21:17 +0530

    Add User class with password protection methods

--- 22.py
+++ 22.py
@@ -26,3 +26,19 @@ car = Car()
 car.start_engine()  # Abstracts complex internal workings
 car.accelerate()
 car.brake()
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.__password = password  # Private attribute
+
+    def get_username(self):
+        return self.username
+
+    def check_password(self, password):
+        return password == self.__password
+
+user = User("dev_karnataka", "pass1234")
+print(user.get_username())  # Access allowed
+print(user.check_password("wrong_pass"))  # Returns False
+print(user.check_password("pass1234"))  # Returns True
