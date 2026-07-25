16c6bac0180ddfdd551e8552586f132834690afa
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-25 21:20:19 +0530

    Update 22.py

--- 22.py
+++ 22.py
@@ -11,3 +11,18 @@ Student = Student()
 Student.student_marks()
 Student.student_name()
 Student.student_age()
+
+class Car:
+    def start_engine(self):
+        print("Engine started")
+
+    def accelerate(self):
+        print("Car accelerating")
+
+    def brake(self):
+        print("Car stopping")
+
+car = Car()
+car.start_engine()  # Abstracts complex internal workings
+car.accelerate()
+car.brake()
