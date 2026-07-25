9fe7bf404154c1a7acc8f1f09568850e774d5032
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-25 21:18:20 +0530

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,13 @@
+class Student:
+    def student_marks(self):
+        print("student marks are 99")
+    def student_name(self):
+        print("student name tarun")
+    def student_age(self):
+        print("student age is 15")
+
+Student = Student()
+
+Student.student_marks()
+Student.student_name()
+Student.student_age()
