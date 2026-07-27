f5423731bcfc9c61fb7cbaf765ac64282774772c
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-27 23:27:32 +0530

    Update 22.py

--- 22.py
+++ 22.py
@@ -107,3 +107,15 @@ class Phone:
 
 phone = Phone()
 phone.call()
+
+class Bank:
+    def __init__(self):
+        self.__balance = 5000
+    def deposit(self, amount):
+        self.__balance += amount
+    def show(self):
+        print("Balance:", self.__balance)
+
+b = Bank()
+b.deposit(1000)
+b.show()
