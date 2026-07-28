fa1aa788f85803cbd40dea53e9a477e8e038c0e8
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-28 22:15:19 +0530

    Add Bank class with balance management

--- 22.py
+++ 22.py
@@ -193,3 +193,10 @@ class Animal(ABC):
 class Dog(Animal):
     def sound(self):
         print("Bark")
+
+class Bank:
+    def __init__(self):
+        self.__balance = 1000
+
+    def get_balance(self):
+        return self.__balance
