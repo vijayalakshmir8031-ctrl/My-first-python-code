c76f95ce8730941095141c7be2f59f723656bbd3
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-30 22:25:07 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -42,3 +42,15 @@ c = 9
 print(a > 0 and b > 6) 
 print(a > 2 or c > 4)
 print(not(a > 2))
+#membership operator
+s = "vijayalakshmi"
+s2 = "vijayalakshmir"
+print("v" in s) and ("v" in s2)
+print("r" in s)  or ("r" in s2)
+print(not("r" in s2))
+my_list = [1, 2, 3, 4, 5]
+my_string = "python"
+print("p" in my_string)
+print(4 in my_list)
+print(9 in my_list)
+print("r" in my_string)
