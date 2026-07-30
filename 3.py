ff8548e2acbe172936294ebadf56477aed79bf12
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-07-30 22:25:25 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -54,3 +54,13 @@ print("p" in my_string)
 print(4 in my_list)
 print(9 in my_list)
 print("r" in my_string)
+
+#comparison operators
+a = 10
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a <= b)
+print(a >= b)
