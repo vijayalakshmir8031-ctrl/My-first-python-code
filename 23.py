f9cd43d2019777496873b3a341722938add1631c
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-08-06 22:11:18 +0530

    Add Dog and Cat classes with sound methods

--- 23.py
+++ 23.py
@@ -175,3 +175,17 @@ class Bank:
 
 bank1 = Bank()
 bank1.show_balance()
+
+class Dog:
+    def sound(self):
+        print("Dog says: Bark")
+
+class Cat:
+    def sound(self):
+        print("Cat says: Meow")
+
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()
