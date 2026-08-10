6e0a05bdee158f8c031ad2296ce7d53029497dbe
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-08-10 21:02:44 +0530

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,8 @@
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+except ValueError:
+    print("Please enter a valid number.")
