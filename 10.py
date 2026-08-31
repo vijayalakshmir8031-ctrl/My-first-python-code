ee5a261d1df531b70bde04e2ec9a6bdeefb6b1b5
Vijayalakshmi R <vijayalakshmir8031@gmail.com>
2026-08-31 22:39:15 +0530

    Add input for largest number comparison

--- 10.py
+++ 10.py
@@ -78,5 +78,13 @@ if num % 2 == 0:
 else:
     print("Odd")
 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest =", a)
+else:
+    print("Largest =", b)
+
 
 
